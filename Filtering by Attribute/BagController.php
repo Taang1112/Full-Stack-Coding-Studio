7eb6a7ebc385951b319325979ce7428609fd9cbc
Taang1112 <smkiu.005@gmail.com>
2025-08-08 08:48:47 +0700

    new

---
+++ Filtering by Attribute/BagController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Bag;
+
+class BagController extends Controller
+{
+    public function submit(Request $request)
+    {
+        $bag = new Bag();
+        $bag->brand = $request->brand;
+        $bag->bahan = $request->bahan;
+        $bag->save();
+
+
+        return redirect()->route('bag');
+    }
+    public function tampil(){
+        $bag = Bag::whereBetween('id', ['2', '4'])->get();
+        return view('semua', ['bag' => $bag]);
+    }
+}
---
+++ Paginate/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BagController;
+use App\Bag;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('bag');
+})->name('bag');
+Route::post('/submit', [BagController::class, 'submit' ])->name('bag.submit');
+Route::get('/semua', [BagController::class, 'tampil' ])->name('bag.tampil');
+
